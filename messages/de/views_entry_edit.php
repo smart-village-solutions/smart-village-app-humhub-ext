7505688dc311b1d5ee492c9809f3ec322310c648
maria sole cincis <msc@tpwd.de>
2022-07-29 13:18:03 +0200

    feat: updated text on line 7, 9, 10

---
+++ messages/de/views_entry_edit.php
@@ -0,0 +1,15 @@
+<?php
+return array (
+  '<strong>Create</strong> event' => 'Termin <strong>erstellen</strong>',
+  '<strong>Edit</strong> event' => 'Termin <strong>bearbeiten</strong>',
+  '<strong>Edit</strong> recurring event' => '<strong>Wiederkehrenden Termin</strong> bearbeiten',
+  'Basic' => 'Allgemein',
+  'Everybody can participate' => 'Jeder darf teilnehmen',
+  'Files' => 'Dateien',
+  'No participants' => 'Jeder kann daran interessiert sein',
+  'Participation' => 'Partizipation',
+  'Recurrence' => 'Wiederkehrende Termine',
+  'Reminder' => 'Erinnerung',
+  'Select event type...' => 'Kategorie wählen',
+  'Title' => 'Titel',
+);
