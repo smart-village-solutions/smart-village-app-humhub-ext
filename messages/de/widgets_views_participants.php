434f831f6139e49834c95ed1d856002b9c7f26d3
maria sole cincis <msc@tpwd.de>
2022-07-29 13:19:11 +0200

    feat: update text on line 6

---
+++ messages/de/widgets_views_participants.php
@@ -0,0 +1,7 @@
+<?php
+return array (
+  ':count attending' => ':count Zusagen',
+  ':count declined' => ':count Ablehnungen',
+  ':count maybe' => ':count Vielleicht',
+  'Participants' => 'Interessierte',
+);
