8bb91275a8a0c793abe889614246b729a9bd65fb
Gaurav Kumar <gaurav.handysolver@gmail.com>
2022-08-08 15:39:10 +0530

    Every registered user should be able to access membership lists in order to find out if member or not #45
    Now, Get the list of members present/exists in the space.

--- controllers/space/MembershipController.php
+++ controllers/space/MembershipController.php
@@ -3,6 +3,7 @@
 namespace humhub\modules\smartVillage\controllers\space;
 
 use humhub\modules\rest\components\BaseController;
+use humhub\modules\rest\definitions\SpaceDefinitions;
 use humhub\modules\space\models\Membership;
 use humhub\modules\space\models\Space;
 use humhub\modules\user\models\User;
@@ -40,4 +41,31 @@ class MembershipController extends BaseController
              return $this->returnError(400,"User is already member of that space!");
          }
      }
+
+    /**
+     * @param $spaceId
+     * @return mixed
+     * Get list of all the member present/exists in the space
+     */
+    //Get the list of all member of a space (issue-45 new endpoint)
+    public function actionIndex($spaceId){
+        $space = Space::findOne(['id'=> (int)$spaceId]);
+
+        if($space == null){
+            return $this->returnError(404, 'Space not found!');
+        }
+
+        $query = Membership::find()->where(['space_id' => (int)$spaceId]);
+
+        if($query->count() == 0){
+            return $this->returnError(404,'No member is exist in space : '.$spaceId);
+        }
+        $results = [];
+        $pagination = $this->handlePagination($query);
+        foreach ($query->all() as $membership){
+            /** @var Membership $membership */
+            $results[] = SpaceDefinitions::getSpaceMembership($membership);
+        }
+        return $this->returnPagination($query,$pagination,$results);
+    }
 }
